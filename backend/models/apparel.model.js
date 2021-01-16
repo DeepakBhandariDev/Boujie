85508535895e6d178f9dac0bbbde56fe44cab596
Deepak Bhandari <Deepbh.forever@gmail.com>
2021-01-16 04:38:02 +0200

    bck

---
+++ backend/models/apparel.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const apparelSchema = new Schema({
+    username: {type: String, required: true},
+    id: {type: Number, required: true},
+    description: {type: String, required: true},
+    colors: {type: String, required: true},
+    sizes: {type: String, required: true},
+    price: {type: Number, required: true},
+    count: {type: Number, required: true},
+    date: {type: Date, required: true}
+
+    
+},{
+    timestamps: true
+}) 
+
+const Apparel = mongoose.model('Apparel', apparelSchema);
+module.exports = Apparel;
---
+++ backend/routes/apparels.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+let Apparel = require('../models/apparel.model');
+
+
+router.route('/').get((req,res) => {
+    Apparel.find()
+    .then(apparels => res.json(apparels))
+    .catch(err => res.status(400).json('Error: '+ err));
+})
+
+router.route('/add').post((req,res) => {
+    const username = req.body.username;
+    const id = req.body.id;
+    const description = req.body.description;
+    const colors = req.body.colors;
+    const sizes = req.body.username;
+    const price = Number(req.body.price);
+    const count = Number(req.body.count);
+    const newApparel = new Apparel({username, id, description, colors, sizes, price, count});
+
+    newApparel.save()
+    .then(() => res.json('Exercise added.'))
+    .catch(err => res.status(400).json('Error: '+ err));
+})
+
+router.route('/:id').get((req,res) => {
+    Apparel.findById(req.params.id)
+    .then(apparel => res.json(apparel))
+    .catch(err => res.status(400).json('Error: '+ err));
+})
+
+router.route('/:id').delete((req,res) => {
+    Apparel.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Apparel deleted'))
+    .catch(err => res.status(400).json('Error: '+ err));
+})
+
+router.route('/update/:id').post((req,res) => {
+    Apparel.findById(req.params.id)
+    .then(apparel => {
+    apparel.username = req.body.username;
+    apparel.id = req.body.id;
+    apparel.description = req.body.description;
+    apparel.colors = req.body.colors;
+    apparel.sizes = req.body.username;
+    apparel.price = Number(req.body.price);
+    apparel.count = Number(req.body.count);
+
+    apparel.save()
+    .then(() => res.json('Apparel updated.'))
+    .catch(err => res.status(400).json('Error: '+ err));
+    })
+    .catch(err => res.status(400).json('Error: '+ err));
+})
+
+module.exports = router;
